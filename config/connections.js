e8cb047bb8a34ecd0b1e592d389e9aaba118d534
danieltjewett <danieltjewett@gmail.com>
2016-03-11 15:51:42 -0500

    switched adapter to sails-mongo

--- config/connections.js
+++ config/connections.js
@@ -58,11 +58,11 @@ module.exports.connections = {
   ***************************************************************************/
   someMongodbServer: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    host: process.env.MONGODB_HOST || 'localhost',
+    port: process.env.MONGODB_PORT || 27017,
+    user: process.env.MONGODB_USERNAME || '',
+    password: process.env.MONGODB_PASSWORD  || '',
+    database: process.env.MONGODB_DATABASE || 'test-app'
   },
 
   /***************************************************************************
